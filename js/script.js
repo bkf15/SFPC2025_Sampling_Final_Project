759250cccd0ed8a67dc1fb0133da364698e4fba1
bkf15 <briankfalkenstein@gmail.com>
2025-11-18 10:10:30 -0500

    Grid wrapping

--- js/script.js
+++ js/script.js
@@ -5,11 +5,12 @@ fetch('projects/index.json')
 
         projects.forEach(p => {
             const div = document.createElement('div');
+            div.classList.add("project-card");
             div.innerHTML = `
-                <h2>${p.title}</h2>
-                <img src="${p.image}" width="300">
-                <audio controls src="${p.song}"></audio>
-                `;
+        <h2>${p.title}</h2>
+        <img src="${p.image}">
+        <audio controls src="${p.song}"></audio>
+      `;
             container.appendChild(div);
         });
     });
